5ff73a6e7f190c8eace507272ddc9acfa2a8ee1b
lopezmarco81 <45408152+lopezmarco81@users.noreply.github.com>
2022-01-05 18:36:59 -0600

    took off Python folder

--- Python/Activities/03-Ins_Variables/Solved/Inclass - names .py
+++
@@ -1,9 +0,0 @@
-name = "Diana Chaves"
-country = "Chicago, IL USA"
-hourly_wage = 65.00
-daily_wage = hourly_wage * 8
-
-satisfied = True
-
-print(f'{name} lives in {country} and she makes {daily_wage} a day, or {hourly_wage} an hour.  She is satisfied {satisfied}' '')
-
