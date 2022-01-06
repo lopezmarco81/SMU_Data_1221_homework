ec82a6919dfd26f2d450c70233aba79cdb55c09c
lopezmarco81 <45408152+lopezmarco81@users.noreply.github.com>
2022-01-06 09:05:39 -0600

    trying to add the submissions folder

---
+++ Python/Activities/10-Stu_RockPaperScissors/Unsolved/RPS_Unsolved copy.py
@@ -0,0 +1,22 @@
+# Incorporate the random library
+import random
+
+# Print Title
+print("Let's Play Rock Paper Scissors!")
+
+# Specify the three options
+options = ["r", "p", "s"]
+
+# Computer Selection
+computer_choice = random.choice(options)
+
+# User Selection
+user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ")
+
+# Run Conditionals
+r = "rock"
+s = "scissors"
+p = "paper"
+if computer_choice 
+elif computer_choice 
+elif
