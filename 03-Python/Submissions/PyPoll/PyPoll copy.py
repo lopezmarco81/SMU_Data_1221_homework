4189ca8acf696bb62635090234f8d0c0ffd4bf43
lopezmarco81 <45408152+lopezmarco81@users.noreply.github.com>
2022-01-15 06:12:37 -0600

    adding progressive changes

--- 03-Python/Submissions/PyPoll/PyPoll copy.py
+++ 03-Python/Submissions/PyPoll/PyPoll copy.py
@@ -4,9 +4,15 @@ import csv
 #File path's 
 csvpath = "PyPoll/Resources/election_data.csv"
 
+#Counting total votes starting at zero
+totalVotes = 0
+
+#Initializing a set - unique list
+candidates = set()
 
 #Opening and Reading file
 with open(csvpath, "r") as file:
+
     csvreader = csv.reader(file, delimiter=',')
     
     csvheader = next(csvreader)
@@ -14,7 +20,15 @@ with open(csvpath, "r") as file:
     print(csvheader)
     print()
     
-  #Iterating ("Looping") through the rows  
+  #Iterating ("Looping") through the rows - Not printing the rows because it is massive
     for row in csvreader:
-  #Not printing the rows because it is massive
-        print(row)
+     # print(row)
+
+#Adding a one to each row of total votes
+      totalVotes += 1
+#Adjoining Index 2 (Candidates Column) the unique list to the set
+      candidates.add(row[2])
+
+#Printouts
+print(totalVotes)
+print(candidates)
