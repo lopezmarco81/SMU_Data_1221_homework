cf0572e9a0843fa9fee4e8151ac42fcc65792354
lopezmarco81 <45408152+lopezmarco81@users.noreply.github.com>
2022-01-15 07:54:22 -0600

    final submissions

--- 03-Python/Submissions/PyPoll/PyPoll.py
+++ 03-Python/Submissions/PyPoll/PyPoll.py
@@ -4,28 +4,20 @@ import csv
 #File path's 
 csvpath = "PyPoll/Resources/election_data.csv"
 
+#Counting total votes starting at zero
 totalVotes = 0
-contenders = 0
 
-#Creating the entire list of contenders
-contenders = set()
-contenders2 = []
-
-
-#Creating a dictionary structure.
-# Per Alexander's comment, this requires for you to know your data before setting this up
-# contender_dict = {
-#     "Khan": 0,
-#     "Correy": 0,
-#     "Li": 0,
-#     "O'Tooley":0
-# }
-#Per Alexander, you can create an open dictionary to include any new candidates
-contenders_dict = {}
-
+#creating a dictionary data structure
+candidate_dict2 = {
+"Khan": 0,
+"Correy": 0,
+"Li": 0,
+"O'Tooley":0
+}
 
 #Opening and Reading file
 with open(csvpath, "r") as file:
+
     csvreader = csv.reader(file, delimiter=',')
     
     csvheader = next(csvreader)
@@ -33,76 +25,55 @@ with open(csvpath, "r") as file:
     print(csvheader)
     print()
     
-  #Iterating ("Looping") through the rows  
+  #Iterating ("Looping") through the rows - Not printing the rows because it is massive (3.5m)
     for row in csvreader:
+     # print(row)
 
-  #Not printing the rows because it is massive
-        #print(row)
-        
-        totalVotes += 1
-        
-        #Adding Index 2
-        contenders.add(row[2])
+#Adding one vote to each row of total votes
+      totalVotes += 1
 
-        if row[2] not in contenders2:
-            contenders2.append(row[2])
+#If candidate does not exist in list
+      if row[2] not in candidate_dict2:
+            candidate_dict2.append(row[2])
 
-    #Per Alexander's suggestion use the simplified instead of the Ifs' statements   
-        #contender_dict[row[2]] +=1
+#Per Alexander's suggestion, this is assuming the candidate is the in the list      
+      candidate_dict2[row[2]] += 1
 
-    # if row[2] == "Khan":
-    #     contender_dict["Khan"] += 1
+#If candidate exists, add 1 to value
+      if row[2] in candidate_dict2.keys():
+            candidate_dict2[row[2]] += 1
+      else:
+            candidate_dict2[row[2]] = 1
 
-    # elif row[2] == "Correy":
-    #     contender_dict["Correy"] += 1
-    
-    # elif   row[2] == "Li":
-    #     contender_dict["Li"] += 1
-    
-    # else:  row[2] == "O'Tooley"
-    # contender_dict["O'Tooley"] += 1   
-    
-    if row[2] in contenders_dict.keys():
-      contenders_dict[row[2]] += 1
-    #Initializing in the dictionary with one vote
-    else:
-        contenders_dict[row[2]] = 1
-
-        
-#Print outs
+#Printouts
 print(totalVotes)
-print(contenders)
-print(contenders_dict)
+print()
+print(candidate_dict2)
 print()
 
-#Print out of Summary Table
+#Getting key with maximum value in dictionary
+max_cand = max(candidate_dict2, key=candidate_dict2.get)
+max_votes = candidate_dict2[max_cand]
 
-max_cont = max(contenders_dict, key=contenders_dict.get)
-max_votes = contenders_dict[max_cont]
+#Summary printout
 
 
-#Print out the Summary Table
 summary = f"""Poll Results
-----------------------------
+--------------------------
 Total Votes: {totalVotes}
-----------------------------\n"""
-for person in contender_dict.keys():
-    prcnt =  100*{contender_dict[person] / totalVotes}
-    summary += f"{person}: {round(prcnt, 2)}% ({contender_dict[person]})\n"
+--------------------------\n"""
 
-summary += f"""---------------------------
------------------------
- {max_cont}:  Elected
- --------------------------
- """
+for person in candidate_dict2.keys():
+    prcnt = 100*(candidate_dict2[person] / totalVotes)
+    summary += f"{person}: {round(prcnt,2)}% ({candidate_dict2[person]})\n"
+
+summary += f"""--------------------------------------
+    Elected:  {max_cand}
+--------------------------------------
+    """
 
 print(summary)
 
-#Writes the text file
-with open("PyPoll_summary.txt", "w") as file:
-    file.write(summary)
-
-#Sources:
-#Maximum value in dictiorary - https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
-#Dividing each dictionary value by total value - https://stackoverflow.com/questions/30964577/divide-each-python-dictionary-value-by-total-value/30964739
-#Resolving End of Line (EOL) Syntax Error - http://net-informations.com/python/err/eol.htm
+#write to file
+with open("PyPoll_Analysis.txt", "w") as file:
+      file.write(summary)
