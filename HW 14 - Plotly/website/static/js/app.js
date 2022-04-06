d3dc434753eed030bf32a594e3ef498f4ff2a9d9
lopezmarco81 <45408152+lopezmarco81@users.noreply.github.com>
2022-04-05 22:04:10 -0500

    Final_Submissions

---
+++ HW 14 - Plotly/website/static/js/app.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+    console.log("Page Loaded");
+    doWork();
+});
+
+function doWork() {
+    // Problem 1: Can I read in the data and then print?
+    var url = "static/data/samples.json";
+    requestAjax(url);
+}
+
+function requestAjax(url) {
+    $.ajax({
+        type: "GET",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+            console.log(data);
+            createDropdown(data);
+            createMetadata(data);
+            createBarChart(data);
+            createBubbleChart(data);
+        },
+        error: function(textStatus, errorThrown) {
+            console.log("FAILED to get data");
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
+}
+
+function requestD3(url) {
+    d3.json(url).then(function(data) {
+        console.log(data);
+
+        createDropdown(data);
+        createMetadata(data);
+        createBarChart(data);
+        createBubbleChart(data);
+    });
+}
+
+function createDropdown(data) {
+    var names = data.names;
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i];
+        let html = `<option>${name}</option>`;
+        $("#selDataset").append(html);
+    }
+}
+
+function createMetadata(data) {
+    let id = $("#selDataset").val();
+    let info = data.metadata.filter(x => x.id == id)[0];
+    console.log(info);
+    Object.entries(info).map(function(x) {
+        let html = `<h6>${x[0]}: ${x[1]}</h6>`;
+        $("#sample-metadata").append(html);
+    });
+}
+
+function createBarChart(data) {
+    let id = $("#selDataset").val();
+    let sample = data.samples.filter(x => x.id == id)[0];
+
+    var trace1 = {
+        type: 'bar',
+        x: sample.sample_values.slice(0, 10).reverse(),
+        y: sample.otu_ids.map(x => `OTU ${x}`).slice(0, 10).reverse(),
+        text: sample.otu_labels.slice(0, 10).reverse(),
+        orientation: 'h'
+    }
+
+    var data1 = [trace1];
+    var layout = {
+        "title": "Bar Chart Placeholder"
+    }
+
+    Plotly.newPlot('bar', data1, layout);
+}
+
+function createBubbleChart(data) {
+    let id = $("#selDataset").val();
+    let sample = data.samples.filter(x => x.id == id)[0];
+
+    var trace1 = {
+        x: sample.otu_ids,
+        y: sample.sample_values,
+        text: sample.otu_labels.slice(0, 10).reverse(),
+        mode: 'markers',
+        marker: {
+            size: sample.sample_values
+        }
+    }
+
+    var data1 = [trace1];
+    var layout = {
+        "title": "Bubble Chart Placeholder"
+    }
+
+    Plotly.newPlot('bubble', data1, layout);
+}
